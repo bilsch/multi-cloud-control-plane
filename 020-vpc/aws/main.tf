0158af5bb74ee8593326dc4e3653fd7cdff3f15f
Bill Schwanitz <bilsch@bilsch.org>
2024-10-24 10:45:33 -0400

    fix path and config variables

--- 020-vpc/aws/main.tf
+++ 020-vpc/aws/main.tf
@@ -1,19 +1,19 @@
 locals {
   # TODO this path sucks!
-  config = yamldecode(file("/Users/bilsch/gits/multi-cloud-control-configs-private/clouds/aws/${var.profile}.yaml"))
+  config = yamldecode(file("/home/bilsch/gits/multi-cloud-control-configs-private/clouds/aws/${var.profile}.yaml"))
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.profile
-  cidr = config.vpc.cidr
+  cidr = local.config.vpc.cidr
 
-  azs              = config.provider.azs
-  private_subnets  = config.vpc.private_subnets
-  database_subnets = config.vpc.database_subnets
-  public_subnets   = config.vpc.public_subnets
-  intra_subnets    = config.vpc.kubernetes_subnets
+  azs              = local.config.provider.azs
+  private_subnets  = local.config.vpc.private_subnets
+  database_subnets = local.config.vpc.database_subnets
+  public_subnets   = local.config.vpc.public_subnets
+  intra_subnets    = local.config.vpc.kubernetes_subnets
 
   enable_nat_gateway     = true
   single_nat_gateway     = false
