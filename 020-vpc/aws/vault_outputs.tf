4ab824464ab1ab466099988c83ec38505168756a
Bill Schwanitz <bilsch@bilsch.org>
2024-10-27 14:12:07 -0400

    set outputs into vault - minimal

--- 020-vpc/aws/vault_outputs.tf
+++ 020-vpc/aws/vault_outputs.tf
@@ -1,19 +1,19 @@
-# resource "vault_kv_secret_v2" "terraform_state_store" {
-#   mount               = "secret"
-#   name                = "bilsch/aws/${var.profile}/terraform_state_store"
-#   cas                 = 1
-#   delete_all_versions = true
+resource "vault_kv_secret_v2" "this" {
+  mount               = "secret"
+  name                = "bilsch/aws/${var.profile}/vpc"
+  cas                 = 1
+  delete_all_versions = true
 
-#   data_json = jsonencode({
-#     "s3_bucket_arn"                = module.s3_bucket.s3_bucket_arn,
-#     "s3_bucket_bucket_domain_name" = module.s3_bucket.s3_bucket_bucket_domain_name,
-#     "s3_bucket_id"                 = module.s3_bucket.s3_bucket_id
-#   })
+  data_json = jsonencode({
+    # https://github.com/terraform-aws-modules/terraform-aws-vpc/blob/master/outputs.tf
+    "nat_public_ips"      = module.vpc.nat_public_ips,
+    "vpc_ipv6_cidr_block" = module.vpc.vpc_ipv6_cidr_block,
+  })
 
-#   custom_metadata {
-#     max_versions = 5
-#     data = {
-#       profile = var.profile,
-#     }
-#   }
-# }
+  custom_metadata {
+    max_versions = 5
+    data = {
+      profile = var.profile,
+    }
+  }
+}
