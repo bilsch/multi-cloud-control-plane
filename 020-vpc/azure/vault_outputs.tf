28e328a0e1af179117e61556b9b57afca4044d57
Bill Schwanitz <bilsch@bilsch.org>
2024-10-29 20:24:11 -0400

    tweak azure vpc output - we only need the public ip in vault

--- 020-vpc/azure/vault_outputs.tf
+++ 020-vpc/azure/vault_outputs.tf
@@ -5,20 +5,6 @@ resource "vault_kv_secret_v2" "this" {
 
   data_json = jsonencode({
     # https://github.com/bilsch/terraform-azure-vpc/blob/main/output.tf
-    "vpc_id"                       = module.vpc.vpc_id,
-    "vpc_name"                     = module.vpc.vpc_name,
-    "vpc_subnet"                   = module.vpc.vpc_subnet,
-    "public_ids"                   = module.vpc.public_ids,
-    "public_names"                 = module.vpc.public_names,
-    "public_service_endpoints"     = module.vpc.public_service_endpoints,
-    "private_ids"                  = module.vpc.private_ids,
-    "private_names"                = module.vpc.private_names,
-    "private_service_endpoints"    = module.vpc.private_service_endpoints,
-    "database_ids"                 = module.vpc.database_ids,
-    "database_names"               = module.vpc.database_names,
-    "database_service_endpoints"   = module.vpc.database_service_endpoints,
-    "kubernetes_ids"               = module.vpc.kubernetes_ids,
-    "kubernetes_names"             = module.vpc.kubernetes_names,
-    "kubernetes_service_endpoints" = module.vpc.kubernetes_service_endpoints,
+    "nat_gateway_public_ip" = module.vpc.nat_gateway_public_ip,
   })
 }
